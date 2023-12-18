3ea4c445ea92cc5c251ab06d90c564e4f5d01f38
nstta <148521410+nsttamon@users.noreply.github.com>
2023-12-19 00:15:16 +0700

    update user

--- authentication.js
+++ authentication.js
@@ -126,7 +126,8 @@ const register = async (req, res) => {
 };
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers['authorization'].split(' ')[1];
+  const decoded = jwt.decode(req.headers['authorization'].split(' ')[1]);
 
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized: Token not provided.' });
